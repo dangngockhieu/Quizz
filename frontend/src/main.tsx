ec574cc01cde26ff334ee3e07943262a56c5c9bc
dangngockhieu <dangngockhieu28082005@gmail.com>
2026-03-02 22:11:36 +0700

    add redux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import { PersistGate } from 'redux-persist/integration/react'
+import App from './App'
+import { store, persistor } from './redux/store'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root')
+
+if (container) {
+  const root = createRoot(container)
+
+  root.render(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <React.StrictMode>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+          <ToastContainer position="top-right" autoClose={3000} />
+        </React.StrictMode>
+      </PersistGate>
+    </Provider>,
+  )
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  )
+}
